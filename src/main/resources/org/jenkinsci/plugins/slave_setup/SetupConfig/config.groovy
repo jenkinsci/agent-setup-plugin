7e390dd0fce9fda9a3cfe5cac2c63e59ca005101
Frederik Fromm <fred@debianmaster.cassini.de>
2012-02-09 18:19:52 +0100

    updated text output in config page

--- src/main/resources/org/jenkinsci/plugins/slave_setup/SetupConfig/config.groovy
+++ src/main/resources/org/jenkinsci/plugins/slave_setup/SetupConfig/config.groovy
@@ -1,15 +1,22 @@
 package org.jenkinsci.plugins.slave_setup.SetupConfig
 
-def f=namespace(lib.FormTagLib)
+def f = namespace(lib.FormTagLib)
 
-f.section(title:_("Slave setup script")) {
-    f.entry(title:_("Setup files directory"), field:"filesDir") {
+f.section(title: _("Slave setup script")) {
+    f.entry(title: _("Setup files directory"), field: "filesDir") {
         f.textbox()
     }
-    f.entry(title:_("Command line"), field:"commandLine") {
+
+    f.entry(title: _("Command line"), field: "commandLine") {
         f.expandableTextbox()
     }
-    f.entry(title:_("Deploy on save to all currently active Slaves (ATTENTION, slaves should be of one kind of System as the is only one script for all!)"), field:"deployNow") {
-       f.checkbox()
+
+
+    f.entry(title: _("Deploy on save to all currently active Slaves"), field: "deployNow") {
+        f.checkbox()
     }
+
+    f.block("<b>(ATTENTION, slaves should be of one kind of System " +
+                "as there is only one script for all!)</b>")
+
 }
