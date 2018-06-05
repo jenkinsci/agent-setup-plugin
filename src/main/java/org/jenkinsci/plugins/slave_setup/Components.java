3ff326b0693d443ebacf4d7afd0c146f66c79022
atgiovannini <djtenzing@gmail.com>
2018-06-05 12:17:13 +0200

    Remove unused imports Update code comment Upgraded newDeploy to be dinamically and not store nothing Updated verbose data Added at end Clearing Temporally folder Better error handling by throwing to Jenkins in most cases

--- src/main/java/org/jenkinsci/plugins/slave_setup/Components.java
+++ src/main/java/org/jenkinsci/plugins/slave_setup/Components.java
@@ -1,15 +1,9 @@
 package org.jenkinsci.plugins.slave_setup;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import com.kenai.jffi.Util;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.SystemUtils;
@@ -21,74 +15,81 @@ import hudson.model.Computer;
 import hudson.model.TaskListener;
 
 /**
- * Vamos a cahcear el delmitador de master y el delimitador de cada nodo
+ * Vamos a cachear el delmitador de master y el delimitador de cada nodo
  * mientras estamos ahaciendo su despliegue
+ * 
+ * We are going to store in cache the master delimiter and each node delimiter
+ * while they are deployed.
+ * @author ByteHeed
  */
 
 public class Components {
 
     private final String FILENAME = "slave_setup.ini";
 
-    // Temporall save variables until Class is finished
     private FilePath remotePath;
-    private List<SetupConfigItem> configs; // I want this object to be null until constructor sets
+    private List<SetupConfigItem> configs; 
     private Computer slave;
     private static TaskListener listener;
 
-    private boolean newDeploy = false;
     private List<String> cache;
     private static boolean debugMode = false;
     private FilePath configFile;
-    private String localSeparator;
     private String remoteSeparator;
 
     /**
      * 
+     * TODO: Add contruction description Check if config exists and create, then
+     * store al remote objects to be used at deploy time
+     * 
+     * @throws InterruptedException
+     * @throws IOException
+     * 
      */
-    public Components(FilePath remoteRootPath, Computer slave) {
-        // store into memory full remote path of config File
+    public Components(FilePath remoteRootPath, Computer slave) throws IOException, InterruptedException {
         this.remotePath = remoteRootPath;
+
         this.configFile = this.remotePath.child(FILENAME);
-        try {
-            if (!this.configFile.exists()) {
-                this.configFile.write();
-                Components.println("New config created on " + this.configFile.getRemote());
-                newDeploy = true;
-            }
-        } catch (IOException e) {
-            Components.info(e.getMessage());
-        } catch (InterruptedException e) {
-            Components.info(e.getMessage());
+        if (!this.configFile.exists()) {
+            this.configFile.write();
+            Components.debug("New config created on " + this.configFile.getRemote());
         }
 
-        this.remoteSeparator = Utils.osLineSeparator(this.remotePath.toString());
+        this.remoteSeparator = Utils.osLineSeparator(this.remotePath.getRemote());
         this.slave = slave;
         this.configs = SetupConfig.get().getSetupConfigItems();
-        this.localSeparator = Utils.osLineSeparator(SystemUtils.IS_OS_UNIX);
 
     }
 
-    // If we want verbose just set listener and verbose will work
+    /**
+     * Check if slave contains some setup configured false, means that is first time
+     * connection or any designed setup // If we want verbose just set listener and
+     * verbose will work
+     */
+    public boolean newDeploy() {
+        return this.getCache().size() > 0;
+    }
+
+    /**
+     * Required method, to define logging procedures
+     */
     public static void setLogger(TaskListener listener) {
         Components.listener = listener;
     }
 
-    // Handle logger and write mensagge into it if exists
-
-    // When you want to access cache you can access with this function, making
-    // always one object,
-    // with no elements if cache is empty This will enable to work with cache always
-    // without errors.
+    /**
+     * Returns current cache, if null will create one empty
+     */
     private List<String> getCache() {
         if (cache == null)
             cache = new ArrayList<String>();
         return cache;
     }
 
-    private void setCache(List<String> newCache) {
-        this.cache = newCache;
-    }
-
+    /**
+     * Append element to cache, and prevent to create duplicateds with diferent
+     * versions or same
+     */
     private void addCache(String component) {
 
         // slow logic to find old version installations
@@ -111,30 +112,27 @@ public class Components {
         }
     }
 
+    /**
+     * Slave setup flow, here, iterate all SetupItems and if not installed jet, will
+     * call doDeploy
+     */
     public boolean doSetup() throws AbortException, IOException, InterruptedException {
-        // Here if newDeploy is false, will read at first time the remote config file.
-        // and store it into cache trhougth overriding cache
-        // Teorically this is first method that will be executed under Components Flow.
-        if (!newDeploy) {
-            Components.debug("Isn't first installation getting cache");
-            // At this point at some time, we do some setup in the past, as well,
-            // need to get installed components from slave,
+        if (!this.newDeploy()) {
+            // If slave contains some setups, will read cache data from slave disk
+
             Components.info("Updating existing installations for " + slave.getName());
             cache = createConfigStream();
-
             Components.debug("Given cache contains this lines:\r\n " + String.join(remoteSeparator, cache));
         } else
             Components.info("Executing first install for " + slave.getName());
 
-        // Now, we will iterate all SetupConfigItems
-        // if item.tagPattern match with Slave.TagString
         for (SetupConfigItem item : configs) {
             if (Utils.labelMatches(item.getAssignedLabelString(), slave)) {
 
                 if (!getCache().contains(item.remoteCache())) {
-                    Components.info("Start setup for " + item.getAssignedLabelString());
+                    Components.info("Installing " + item.getAssignedLabelString());
                     this.doDeploy(item);
-                    Components.info("Setup for " + item.getAssignedLabelString() + " succeded");
+                    Components.info("Install " + item.getAssignedLabelString() + " succeded");
                 } else
                     Components.info(String.format("%s slave have last version of %s", slave.getName(),
                             item.getAssignedLabelString()));
@@ -144,96 +142,128 @@ public class Components {
         return false;
     }
 
+    /**
+     * Entire Setup flow for one Item First, will execute scripts on master Second,
+     * will deploy files to slave Third, Will run slave scrfipts
+     * 
+     * @throws InterruptedException
+     * @throws IOException
+     */
     private void doDeploy(SetupConfigItem installInfo) throws IOException, InterruptedException {
         EnvVars enviroment = SetupDeployer.createEnvVarsForComputer(this.slave);
-        // At first point will create our EnvVars for computers
-        // createEnvVarsforComputer(Computer)
-        // Entire scripts flow execution
-        // if prepare is empty ignore masterExecute
+
         if (!StringUtils.isEmpty(installInfo.getPrepareScript())) {
+            // If isn't empty script will execute on master
             validateResponse(SetupDeployer.executeScriptOnMaster(installInfo.getPrepareScript(), this.slave,
-                    this.listener, enviroment));
+                    Components.listener, enviroment));
             installInfo.setPrepareScriptExecuted(true);
         }
 
-        // copyFiles will handle if emtpy or not is the value
+        // Copy files from master to slave (only if option contains some path)
         SetupDeployer.copyFiles(installInfo.getFilesDir(), remotePath);
 
         if (!StringUtils.isEmpty(installInfo.getCommandLine())) {
-            validateResponse(Utils.multiOsExecutor(listener, installInfo.getCommandLine(), remotePath, enviroment));
+            // If we had slave script, will cal now.
+            validateResponse(
+                    Utils.multiOsExecutor(Components.listener, installInfo.getCommandLine(), remotePath, enviroment));
         }
-
-        // Maybe we need to set prepareScript to true if was installed, but Aaron think
-        // that raise exception is better.
-
-        // First script es prepare under master
-
-        // continue with deploy files to slave
-
-        // Execute script in slave
+        // Add to cache in order to prevent reinstall this version.
         this.addCache(installInfo.remoteCache());
-
     }
 
+    /**
+     * TODO: Update-me Only works on Unix Operating Systems, this will use some unix
+     * commands to clear temporally data
+     * 
+     * If file had more than 5 minutes in last modified tag, from OS, will be
+     * removed Only will remove files containing jenkins in name
+     * 
+     * This function require at least (find) binary to be installed
+     * 
+     * @throws InterruptedException
+     * @throws IOException
+     * @throws AbortException
+     * 
+     */
+    public void clearTemporally() throws AbortException, IOException, InterruptedException {
+
+        String clearTmp = "find /tmp -type f -atime +10 -delete -iname *jenkins*.sh -maxdepth 0";
+        EnvVars environ = SetupDeployer.createEnvVarsForComputer(this.slave);
+
+        if (this.remotePath.toString().startsWith("/")) {
+            // Clear slave temporally data
+            Components.info("Clearing temporally data on " + this.slave.getName());
+            validateResponse(Utils.multiOsExecutor(Components.listener, clearTmp, this.remotePath, environ));
+        }
+
+        if (SystemUtils.IS_OS_UNIX) {
+            // Clear master temporally data
+            Components.info("Clearing temporally data on jenkins master");
+            validateResponse(SetupDeployer.executeScriptOnMaster(clearTmp, this.slave, Components.listener, environ));
+        }
+        Components.debug("Finished temporal data removed");
+    }
+
+    /**
+     * Validate execution scripts code, in order to throw exception if not
+     * 
+     * @throws AbortException
+     */
     private void validateResponse(int r) throws AbortException {
         if (r != 0) {
-            Components.println("ScriptFailed " + r);
+            Components.info("ScriptFailed " + r);
             throw new AbortException("script failed!");
         }
     }
 
-    private void closeConfigStream() {
-        // At this point will close a stream and write all pending data into it.
-        // after data is writen to remote disk close connection (if need it)
-        try {
-            Components.debug(
-                    String.format("Updating %s with\n%s", this.configFile, StringUtils.join(getCache(), "\r\n")));
-            configFile.write(StringUtils.join(cache, this.remoteSeparator), "UTF-8");
-            // FileWriter fileHdl = new FileWriter(configFile.getRemote(), false);
-            // BufferedWriter writer = new BufferedWriter(fileHdl);
-            // writer.write(StringUtils.join(cache, System.getProperty("line.separator")));
-
-        } catch (Exception ex) {
-            Components.println("Components:closeConfigStream" + ex.getMessage());
-        }
-    }
-
-    private List<String> createConfigStream() {
-        // Open Stream to config file, read it, close stream, but not connection
-        //
-        try {
-            return new ArrayList(Arrays.asList(this.configFile.readToString().split(this.remoteSeparator)));
-
-        } catch (Exception ex) {
-            Components.info("Components:createConfigStream::" + ex.getMessage());
-            return null;
-        }
+    /**
+     * Unlink remote file, after write pending cache
+     * 
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    private void closeConfigStream() throws IOException, InterruptedException {
+        Components.debug(String.format("Updating %s with\n%s", this.configFile, StringUtils.join(getCache(), "\r\n")));
+        configFile.write(StringUtils.join(cache, this.remoteSeparator), "UTF-8");
     }
 
     /**
-     * @deprecated use Components.info or Components.debug instead will handle
-     *             stdout if debugMode enabled or not. Actually this is redirected
-     *             to Components.info.
+     * From slave read cache file, and lock it.
+     * 
+     * @throws InterruptedException
+     * @throws IOException
      */
-    @Deprecated
-    public static void println(String message) {
-        Components.info(message);
+    private List<String> createConfigStream() throws IOException, InterruptedException {
+        return new ArrayList<String>(Arrays.asList(this.configFile.readToString().split(this.remoteSeparator)));
+
     }
 
+    /**
+     * Print only if debug enabled (debug purposes)
+     */
     public static void debug(String message) {
         if (Components.debugMode)
             Components.info(message);
     }
 
+    /**
+     * Global printer to return logger to master
+     */
     public static void info(String message) {
         if (Components.listener != null)
             Components.listener.getLogger().println(message);
     }
 
+    /**
+     * Just enable debug mode
+     */
     public static void enableDebug() {
         Components.debugMode = true;
     }
 
+    /**
+     * Just disable debug Mode
+     */
     public static void disableDebug() {
         Components.debugMode = false;
     }
